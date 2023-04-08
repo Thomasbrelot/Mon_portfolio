20ebde79119b0a017a6d72521d6052dcfb43a8a8
thomas <thomas77140@hotmail.com>
2023-04-08 09:46:06 +0200

    premier commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("salut")
+/* texte affichage solo */
+var typed = new Typed('#element', {
+  strings: [
+    "Hi! I'm Thomas",
+    "Salut! Moi c'est Thomas",
+    'Welcome on my portfolio <br> the web développement !',
+    'Bienvenue sur mon portfolio <br> de développeur web !',
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
